3df1be0cdf6a5fc6999f2027d9563d525244c70d
Claudio Marcos de Campos <cmcampos86@gmail.com>
2024-01-24 07:23:45 -0300

    Estudos Angular

---
+++ MeuProjeto/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FooterComponent } from './navegacao/footer/footer.component';
+import { HomeComponent } from './navegacao/home/home.component';
+import { MenuComponent } from './navegacao/menu/menu.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FooterComponent,
+    HomeComponent,
+    MenuComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
